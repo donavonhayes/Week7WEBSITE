d2a2090c9e65ed89513ec8463f2db0ffb054274b
donavonhayes <131027014+donavonhayes@users.noreply.github.com>
2023-06-08 16:02:56 -0500

    9 commit

--- week7website/index.js
+++ week7website/index.js
@@ -1,48 +1,35 @@
-
-// This makes the cursor move to 'new-vehicle-make' text input after submitting current data
-// found solution here: https://www.techiedelight.com/set-focus-to-input-text-box-javascript/
 document.getElementById("add").onclick = function () {
 	document.getElementById("new-Songs").focus();
 }
 
-// This allows the "ENTER" key to be used to submit data from text boxes
 window.addEventListener("keydown", function (event) {
 	if (event.defaultPrevented) {
-		return; // Do nothing if the event was already processed
+		return; 
 	}
 	switch (event.key) {		
 		case "Enter":
 			console.log('-----Enter key was pressed-----');
-			// Do something for "enter" or "return" key press.
-			// allows the "Enter" key to submit all HTML elements tags 'label' to submit data
 			this.document.getElementsByTagName('label');
-			// allows the document to create new container that holds the data in the text boxes
 			this.document.getElementById('add').click();
 			break;
 		default:
-			return; // Quit when this doesn't handle the key event.
+			return; 
 	}
-	// Cancel the default action to avoid it being handled twice
 	event.preventDefault();
 }, true);
 
-// creating variable for assignment 
 let id = 0;
 
 document.getElementById('add').addEventListener('click', () => {
-	// This assigns a new Text node to the variable createdVehicle
     let createdPlaylist = new Text();
 	console.log('variable createdPlaylist empty string =', createdPlaylist);
     
-    // This stamps out a new container from the input text which will include the table header and code below
     let table = document.getElementById('songs-table');
     console.log('variable table = This is new container from input', table);
 
-	// This is row 1 because row 0 is made above, which includes the th as row 0
     let row = table.insertRow(1);
     console.log('variable row = This is adding another row of data from text input', row);
 
-	// This sets row id for the new data, will be referenced by delete button later
     row.setAttribute('id', `item-${id}`);
 	console.log('row.setAttribute: sets new ID to added item', 'id', `item-${id}`)
 
@@ -66,29 +53,22 @@ document.getElementById('add').addEventListener('click', () => {
 
 function createDeleteButton(id) {
 
-	// This creates a new button element
     let btn = document.createElement('button');
 	console.log(document.createElement('button'));
 
-	// This inserts the class of 'btn btn-danger' into the button element
     btn.className = 'btn btn-danger';
 	console.log(btn.className);
 
-	// This assigns an id to the button element
     btn.id = id;
 	console.log(btn.id);
 
-	// This adds the text 'Remove' to the button
     btn.innerHTML = 'Remove';
 	console.log(btn.innerHTML);
 
-	//
     btn.onclick = () => {
 
-		// this find the row associated with the button and assigns it to variable elementToDelete
         let elementToDelete = document.getElementById(`item-${id}`);
 				
-		// this removes the corresponding row the delete button is inside of
         elementToDelete.parentNode.removeChild(elementToDelete);
 		console.log('Deleting :', elementToDelete);
     };
